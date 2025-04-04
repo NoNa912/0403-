a36e1baf7a449ea567ccc58747f37c8e8ed7f955
Nozomi NAMI <ta11kuntal@yahoo.co.jp>
2025-04-04 14:35:36 +0900

    単純なHTML、JAVA、Zipファイルの入ったフォルダを追加

---
+++ my-practice-repo/eclipse-practice/Rensyu13/src/sumplecode_13/CalcTest.java
@@ -0,0 +1,12 @@
+package sumplecode_13;
+
+public class CalcTest {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 3;
+		System.out.println("加算"+ (a + b));
+		System.out.println("減産"+ (a - b));
+		System.out.println("乗算"+ (a * b));
+		System.out.println("除算"+ ((double) a / b));
+	}
+}
